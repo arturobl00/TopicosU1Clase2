1627c5376bd019a4a2999629831276cba04e1609
José Arturo Bustamante Lazcano <arturobl00@msn.com>
2021-02-11 09:00:05 -0600

    Bootstrap

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Topicos Avanzados de Programación</title>
+</head>
+<body>
+    <div class="jumbotron text-center">
+        <h1>Semana 2 Trabajando interfaces con Bootstrap</h1>
+        <p>Este sitio incluye Bootstrap y es un ejemplo del diseño responsive.</p>
+    </div>
+    
+    <br>
+    <nav class="navbar navbar-expand-sm bg-dark">
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Columnas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./textoTipografia.php">TextoTipografia</a>
+    </li>
+  </ul>
+
+</nav>
+
+    <br>
+
+    <h2 class="jumbotron text-center">Tabajando con Columnas<h2>
+
+    <div class="container bg-primary text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-success text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-info text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-warning text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-danger text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-secondary text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container bg-dark text-white">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Columna1</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna2</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Columna3</h3>
+                <p>Lorem ipsum dolor..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-3 my-3 border text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>This container has a border and some extra padding and margins.</p>
+    </div>
+
+    <div class="container p-3 my-3 bg-primary text-white text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>This container has a border and some extra padding and margins.</p>
+    </div>
+
+    <div class="container p-3 my-3 bg-danger text-white text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>This container has a border and some extra padding and margins.</p>
+    </div>
+
+    <h2 class="jumbotron text-center">Tabajando con Columnas<h2>
+
+    <div class="row border">
+        <div class="col-sm-3">.col-sm-3</div>
+        <div class="col-sm-3">.col-sm-3</div>
+        <div class="col-sm-3">.col-sm-3</div>
+        <div class="col-sm-3">.col-sm-3</div>
+    </div>
+
+    <div class="row border">
+        <div class="col-md-3">.col-md-3</div>
+        <div class="col-md-3">.col-md-3</div>
+        <div class="col-md-3">.col-md-3</div>
+        <div class="col-md-3">.col-md-3</div>
+    </div>
+
+    <div class="row border">
+        <div class="col-lg-3">.col-lg-3</div>
+        <div class="col-lg-3">.col-lg-3</div>
+        <div class="col-lg-3">.col-lg-3</div>
+        <div class="col-lg-3">.col-lg-3</div>
+    </div>
+
+    <div class="row border">
+        <div class="col-xl-3">.col-xl-3</div>
+        <div class="col-xl-3">.col-xl-3</div>
+        <div class="col-xl-3">.col-xl-3</div>
+        <div class="col-xl-3">.col-xl-3</div>
+    </div>
+</body>
+</html>
---
+++ textoTipografia.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Topicos Avanzados de Programación</title>
+</head>
+<body>
+    <div class="jumbotron text-center">
+            <h1>Semana 2 Trabajando interfaces con Bootstrap</h1>
+            <p>Este sitio incluye Bootstrap y es un ejemplo del diseño responsive.</p>
+    </div>
+    <br>
+    <nav class="navbar navbar-expand-sm bg-dark">
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link" href="./index.php">Columnas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">TextoTipografia</a>
+    </li>
+  </ul>
+
+</nav>
+
+    <br>
+
+    <h2 class="jumbotron text-center">Tabajando con Textos y Tipografias<h2>
+    <div class="container">
+        <h1>Display Headings</h1>
+        <p>Display headings are used to stand out more than normal headings (larger font-size and lighter font-weight):</p>
+        <h1 class="display-1">Display 1</h1>
+        <h1 class="display-2">Display 2</h1>
+        <h1 class="display-3">Display 3</h1>
+        <h1 class="display-4">Display 4</h1>
+    </div>
+</body>
+</html>
